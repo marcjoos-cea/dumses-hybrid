c70acf8358ab880471853e05949be9754183e23d
Marc Joos <marc.joos@cea.fr>
2022-01-04 10:50:06 +0100

    NCCL implementation + rework of GPU initialization + update of Makefile examples

--- src/subroutines/parallel.f90
+++ src/subroutines/parallel.f90
@@ -79,41 +79,29 @@ subroutine init_parallel
   !         & , provided == MPI_THREAD_MULTIPLE)
   ! endif
 
-# if NCCL == 1
+#if OACC == 1
   call MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, mype, MPI_INFO_NULL &
        , local_comm, ierr)  
   call MPI_Comm_rank(local_comm, local_mype, ierr)
   ierr = cudaSetDevice(local_mype)
-  write(*,*) 'cudasetdevice', ierr
+
   call acc_init(acc_get_device_type())
   call acc_set_device_num(local_mype, acc_get_device_type()) 
-  nthreads = acc_get_num_devices(acc_device_nvidia)
-  
+  nthreads = acc_get_num_devices(acc_get_device_type())
+#endif
+
+# if NCCL == 1
   if (mype == 0) then
      nccl_ierr = ncclGetUniqueId(nccl_uid)
-     write(*,*) 'getuniqueid: ', nccl_ierr
   end if
 
   nccl_uid_internal_size = sizeof(nccl_uid%internal)
   call MPI_Bcast(nccl_uid%internal, nccl_uid_internal_size, MPI_CHAR, 0, MPI_COMM_WORLD, ierr)
-  write(*,*) 'bcast: ', ierr
   
   nccl_ierr = ncclCommInitRank(nccl_comm, npes, nccl_uid, mype)
-  write(*,*) 'comminitrank: ', nccl_ierr
 #endif
 #endif
   
-!#if OACC == 1
-!  call acc_init(acc_device_nvidia)
-!  nthreads = acc_get_num_devices(acc_device_nvidia)
-!  
-!  do i = 0, npes-1
-!     if (i == mype) then
-!        call acc_set_device_num(i, acc_device_nvidia)
-!     endif
-!  enddo
-!#endif
-
   !$OMP PARALLEL
   !$OMP MASTER
   !$ nthreads = omp_get_num_threads()
@@ -194,10 +182,6 @@ subroutine finalize_mpi
 #endif
   
 #if MPI == 1
-  ! With OpenMPI, MPI freezes on MPI_Finalize(); MPI_Abort() helps to quit MPI 
-  ! without freezing, but there must be a cleaner way to do that...
-  ! UPDATE: It actually works with MPICH3, but not with OpenMPI1.x
-  !call MPI_Abort(MPI_COMM_WORLD, error, ierr)
   call MPI_Finalize(ierr)
 #endif
 
@@ -312,13 +296,13 @@ subroutine boundary_x
 #endif
 #if NCCL == 1
      nccl_ierr = ncclGroupStart()
-     nccl_ierr = ncclSend(slbound_x, size, ncclDouble, xleft, nccl_comm, 10)
-     nccl_ierr = ncclRecv(rlbound_x, size, ncclDouble, xright, nccl_comm, 10)
+     nccl_ierr = ncclSend(slbound_x, size, ncclDouble, xleft, nccl_comm, 0)
+     nccl_ierr = ncclRecv(rlbound_x, size, ncclDouble, xright, nccl_comm, 0)
      nccl_ierr = ncclGroupEnd()
 
      nccl_ierr = ncclGroupStart()
-     nccl_ierr = ncclSend(srbound_x, size, ncclDouble, xright, nccl_comm, 11)
-     nccl_ierr = ncclRecv(rrbound_x, size, ncclDouble, xleft, nccl_comm, 11)
+     nccl_ierr = ncclSend(srbound_x, size, ncclDouble, xright, nccl_comm, 0)
+     nccl_ierr = ncclRecv(rrbound_x, size, ncclDouble, xleft, nccl_comm, 0)
      nccl_ierr = ncclGroupEnd()
 #else     
         call MPI_Sendrecv(slbound_x, size, MPI_DOUBLE_PRECISION, xleft, 10 &
@@ -351,6 +335,9 @@ subroutine boundary_x
      endif
   else
      if (nxslice > 1) then
+#if NCCL == 1
+        nccl_ierr = ncclGroupStart()
+#endif
         if (xleft > mype) then
            !$acc kernels loop
            !$OMP PARALLEL DO SCHEDULE(RUNTIME)
@@ -369,10 +356,8 @@ subroutine boundary_x
            !$acc update host(srbound_x)
 #endif
 #if NCCL == 1
-           nccl_ierr = ncclGroupStart()
-           nccl_ierr = ncclSend(srbound_x, size, ncclDouble, xright, nccl_comm, 11)
-           nccl_ierr = ncclRecv(rlbound_x, size, ncclDouble, xright, nccl_comm, 10)
-           nccl_ierr = ncclGroupEnd()
+           nccl_ierr = ncclSend(srbound_x, size, ncclDouble, xright, nccl_comm, 1)
+           nccl_ierr = ncclRecv(rlbound_x, size, ncclDouble, xright, nccl_comm, 0)
 #else     
            call MPI_Sendrecv(srbound_x, size, MPI_DOUBLE_PRECISION, xright, 11 &
                            , rlbound_x, size, MPI_DOUBLE_PRECISION, xright, 10 &
@@ -412,10 +397,8 @@ subroutine boundary_x
            !$acc update host(slbound_x, srbound_x)
 #endif
 #if NCCL == 1
-           nccl_ierr = ncclGroupStart()
-           nccl_ierr = ncclSend(slbound_x, size, ncclDouble, xleft, nccl_comm, 10)
-           nccl_ierr = ncclRecv(rrbound_x, size, ncclDouble, xleft, nccl_comm, 11)
-           nccl_ierr = ncclGroupEnd()
+           nccl_ierr = ncclSend(slbound_x, size, ncclDouble, xleft, nccl_comm, 0)
+           nccl_ierr = ncclRecv(rrbound_x, size, ncclDouble, xleft, nccl_comm, 1)
 #else     
            call MPI_sendrecv(slbound_x, size, MPI_DOUBLE_PRECISION, xleft, 10 &
                            , rrbound_x, size, MPI_DOUBLE_PRECISION, xleft, 11 &
@@ -458,15 +441,11 @@ subroutine boundary_x
            !$acc update host(slbound_x, srbound_x)
 #endif
 #if NCCL == 1
-           nccl_ierr = ncclGroupStart()
-           nccl_ierr = ncclSend(slbound_x, size, ncclDouble, xleft, nccl_comm, 10)
-           nccl_ierr = ncclRecv(rlbound_x, size, ncclDouble, xright, nccl_comm, 10)
-           nccl_ierr = ncclGroupEnd()
+           nccl_ierr = ncclSend(slbound_x, size, ncclDouble, xleft, nccl_comm, 0)
+           nccl_ierr = ncclRecv(rlbound_x, size, ncclDouble, xright, nccl_comm, 0)
 
-           nccl_ierr = ncclGroupStart()
-           nccl_ierr = ncclSend(srbound_x, size, ncclDouble, xright, nccl_comm, 11)
-           nccl_ierr = ncclRecv(rrbound_x, size, ncclDouble, xleft, nccl_comm, 11)
-           nccl_ierr = ncclGroupEnd()
+           nccl_ierr = ncclSend(srbound_x, size, ncclDouble, xright, nccl_comm, 1)
+           nccl_ierr = ncclRecv(rrbound_x, size, ncclDouble, xleft, nccl_comm, 1)
 #else     
            call MPI_Sendrecv(slbound_x, size, MPI_DOUBLE_PRECISION, xleft, 10 &
                            , rlbound_x, size, MPI_DOUBLE_PRECISION, xright, 10 &
@@ -496,6 +475,9 @@ subroutine boundary_x
            enddo
            !$OMP END PARALLEL DO
         endif
+#if NCCL == 1
+        nccl_ierr = ncclGroupEnd()
+#endif
      endif
   endif
 
@@ -539,11 +521,6 @@ subroutine boundary_y
         enddo
      enddo
      !$OMP END PARALLEL DO
-
-     !- DEBUG
-     !$acc update host(slbound_y, srbound_y, rlbound_y, rrbound_y)
-     !write(*,*) "in:", mype, time, slbound_y(iu1+nx/2,ku1+nz/2,1,1)
-     !- DEBUG
      
 #if RDMA == 1
      !$acc host_data use_device(slbound_y, srbound_y, rlbound_y, rrbound_y)
@@ -553,7 +530,6 @@ subroutine boundary_y
 #if NCCL == 1
      nccl_ierr = ncclGroupStart()
      nccl_ierr = ncclSend(slbound_y, size, ncclDouble, yleft, nccl_comm, 0)
-     !write(*,*) mype, nccl_ierr
      nccl_ierr = ncclRecv(rlbound_y, size, ncclDouble, yright, nccl_comm, 0)
      nccl_ierr = ncclGroupEnd()
 
@@ -576,12 +552,6 @@ subroutine boundary_y
      !$acc update device(rlbound_y, rrbound_y)
 #endif
 
-     !- DEBUG
-     !$acc update host(slbound_y, srbound_y, rlbound_y, rrbound_y)
-     !write(*,*) "out:", mype, time, rlbound_y(iu1+nx/2,ku1+nz/2,1,1)
-     !- DEBUG
-
-     
      if ((yposition == (nyslice-1)) .and. &
           (boundary_type(2) /= "periodic")) then
         !$acc kernels loop
@@ -663,13 +633,13 @@ subroutine boundary_z
 #endif
 #if NCCL == 1
      nccl_ierr = ncclGroupStart()
-     nccl_ierr = ncclSend(slbound_z, size, ncclDouble, zleft, nccl_comm, 30)
-     nccl_ierr = ncclRecv(rlbound_z, size, ncclDouble, zright, nccl_comm, 30)
+     nccl_ierr = ncclSend(slbound_z, size, ncclDouble, zleft, nccl_comm, 0)
+     nccl_ierr = ncclRecv(rlbound_z, size, ncclDouble, zright, nccl_comm, 0)
      nccl_ierr = ncclGroupEnd()
 
      nccl_ierr = ncclGroupStart()
-     nccl_ierr = ncclSend(srbound_z, size, ncclDouble, zright, nccl_comm, 31)
-     nccl_ierr = ncclRecv(rrbound_z, size, ncclDouble, zleft, nccl_comm, 31)
+     nccl_ierr = ncclSend(srbound_z, size, ncclDouble, zright, nccl_comm, 0)
+     nccl_ierr = ncclRecv(rrbound_z, size, ncclDouble, zleft, nccl_comm, 0)
      nccl_ierr = ncclGroupEnd()
 #else     
      call MPI_Sendrecv(slbound_z, size, MPI_DOUBLE_PRECISION, zleft, 10 &
